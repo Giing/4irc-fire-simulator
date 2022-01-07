fa4328250fd7ef4b411efff5df4ecc28fb792166
Zennyth <matsfox@orange.fr>
2022-01-07 09:04:03 +0100

    support pending

--- src/main/java/managers/Main.java
+++ src/main/java/managers/Main.java
@@ -1,7 +1,11 @@
 package managers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.PropertiesReader;
 import module.api.Api;
+import module.model.Sensor;
 import module.socket.Events;
 import module.socket.WebSocket;
 
@@ -14,6 +18,15 @@ public class Main {
         PropertiesReader prop = new PropertiesReader();
         Api api = new Api(prop.getProp().getProperty("BASE_URL"), prop.getProp().getProperty("API_KEY"));
         WebSocket ws = new WebSocket(prop.getProp().getProperty("BASE_URL"), prop.getProp().getProperty("WEBSOCKET_KEY"));
+
+        List<Sensor> simulatorSensors = new ArrayList<Sensor>();
+        System.out.println("Pending ...");
+        do
+        {
+            simulatorSensors = api.sensor.getAll();
+        } while (simulatorSensors.isEmpty());
+
+        System.out.println("Connection successful !");
 
 
         FireManager test = new FireManager(api);
